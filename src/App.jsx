caa6e6b3898da43b5a607dfd9fc451fe04398e87
Shepe1304 <hqgiang1346@gmail.com>
2025-03-07 22:56:16 -0500

    First commit, pushing all progress (flashcards finished, now only other features will be added)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Flashcard from "./components/Flashcard";
+
+function App() {
+  return (
+    <div className="App">
+      <Flashcard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Flashcard.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import flashcardsData from "../../public/data.json";
+import "./Flashcard.css";
+import bookClosed from "../assets/images/book-solid.svg";
+import bookOpen from "../assets/images/book-open-solid.svg";
+
+const Flashcard = () => {
+  const { title, description, cards } = flashcardsData;
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [flipped, setFlipped] = useState(false); // Track card flip state
+
+  const shuffleCards = (cards) => {
+    const shuffled = [...cards];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
+
+  const [shuffledCards, setShuffledCards] = useState(shuffleCards(cards));
+
+  const handleNextCard = () => {
+    setTimeout(() => {
+      setCurrentCardIndex((currentCardIndex + 1) % cards.length);
+    }, 200);
+    setFlipped(false);
+  };
+
+  const handlePrevCard = () => {
+    setTimeout(() => {
+      setCurrentCardIndex((currentCardIndex - 1 + cards.length) % cards.length);
+    }, 200);
+    setFlipped(false);
+  };
+
+  return (
+    <div className="flashcard--container">
+      <div className="flashcard--headings">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="flashcard--wrapper">
+        <div
+          className={`flashcard ${flipped ? "flipped" : ""}`}
+          onClick={() => setFlipped(!flipped)} // Toggle flip state on click
+        >
+          <div className="flashcard--inner">
+            <div className="flashcard--front">
+              <div className="flashcard--icon">
+                <img src={bookClosed} alt="Book closed" />
+              </div>
+              <div className="flashcard--content">
+                <div>{shuffledCards[currentCardIndex].question}</div>
+                {shuffledCards[currentCardIndex].type === "image" && (
+                  <div className="flashcard--image">
+                    <img
+                      src={
+                        new URL(
+                          `../assets/images/${shuffledCards[currentCardIndex].image}`,
+                          import.meta.url
+                        ).href
+                      }
+                      alt=""
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="flashcard--difficulty">
+                {shuffledCards[currentCardIndex].difficulty
+                  .charAt(0)
+                  .toUpperCase() +
+                  shuffledCards[currentCardIndex].difficulty
+                    .slice(1)
+                    .toLowerCase()}
+              </div>
+            </div>
+            <div className="flashcard--back">
+              <div className="flashcard--icon">
+                <img src={bookOpen} alt="Book open" />
+              </div>
+              <div className="flashcard--content">
+                {shuffledCards[currentCardIndex].answer}
+              </div>
+              <div className="flashcard--difficulty">
+                {shuffledCards[currentCardIndex].difficulty
+                  .charAt(0)
+                  .toUpperCase() +
+                  shuffledCards[currentCardIndex].difficulty
+                    .slice(1)
+                    .toLowerCase()}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flashcard--buttons">
+        <button className="flashcard--button" onClick={handlePrevCard}>
+          Previous
+        </button>
+        <button className="flashcard--button" onClick={handleNextCard}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Flashcard;
